50a14fdd80f92d89a3e61ececb8a3bea6b3d3720
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2024-10-18 14:30:57 +0700

    api finished

--- InternshipProj/backend/routes/api/api.js
+++ InternshipProj/backend/routes/api/api.js
@@ -5,12 +5,12 @@ const mysql = require('mysql2');
 
 // Create a connection
 const db = mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12718944',
-    password: '1DYCQpAPGS',
-    database: 'sql12718944'
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'localdatabase',
+    port: 3306
 });
-
 // Connect to a database
 db.connect((err) =>{
     if(err){
@@ -20,4 +20,45 @@ db.connect((err) =>{
     console.log('Connected to MySQL')
 });
 
+// Get All user
+rounter.get("/admin", (req, res) => {
+    db.query("SELECT * FROM userinfo", (err, results) =>{
+        // ถ้ามเชื่อมต่อไม่ได้
+        if (err){
+            res.status(400).json({msg : "Database errors. Cannot queries."})
+        }
+        // เชื่อมต่อได้
+        else{
+            res.json(results);
+        }
+    })
+})
+
+// Sign up for user
+rounter.post("/register", (req, res) => {
+    const newUser = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        address: req.body.address,
+        subdistrict: req.body.subdistrict,
+        area: req.body.area,
+        province: req.body.province,
+        postcode: req.body.postcode,
+        phonenum: req.body.phonenum,
+        teamchamp: req.body.teamchamp
+    }
+    console.log(req.body)
+    db.query("INSERT INTO userinfo (firstname, lastname, address, subdistrict, area, province, postcode, phonenum, teamchamp) VALUES (?,?,?,?,?,?,?,?,?)", 
+        [newUser.firstname, newUser.lastname, newUser.address, newUser.subdistrict, 
+            newUser.area, newUser.province, newUser.postcode, newUser.phonenum, newUser.teamchamp], (err, results) =>{
+            if(err){
+                console.log(err);
+                res.status(400).json({msg: "Database error. Cannot insert data."})
+            }
+            else{
+                res.json(results);
+            }
+        })
+})
+// Get All user with filter by Champion
 module.exports = rounter;
